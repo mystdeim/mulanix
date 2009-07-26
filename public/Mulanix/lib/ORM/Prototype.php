5fe4f340efe764991126b7f9ed068644dda0630a
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-07-26 13:23:38 +0000

    * Написаны примеры в докуметацию пакета Mnix_ORM * Изменён дамп тестовых таблиц

--- public/Mulanix/lib/ORM/Prototype.php
+++ public/Mulanix/lib/ORM/Prototype.php
@@ -2,41 +2,86 @@
 /**
  * Mulanix Framework
  *
- * Отношения 1:1
- * Потомок внешний ключ 'fk', указывающий на предка
- * <code>
- * $_has_one = array(
- *     'name' => array (
- *         'class' => 'Class_Name'
- *         'fk'    => 'field'
- *      )
- * )
- * </code>
- *
+ * @category Mulanix
  * @package Mnix_ORM
- * @author deim
- * @copyright 2009
+ * @version 2009-07-25
+ * @since 2008-10-01
  */
 /**
+ * Представляет объектно-ориентированную проекцию Базы Данных
+ *
+ * Объявление объектов осуществляется с помощью добавления защищенных свойтв:
+ * 1. $_table    - название таблицы
+ * 2. $_has_one  - отношение 1:1
+ * 3. $_has_many - отношения 1:N или N:N
+ * Примеры:
+ * Table1 и Table2 в отношии 1:1 или 1:N
+ * <code>
+ * class Table1 extends Mnix_ORM_Prototype
+ * {
+ *     protected $_table = 'table1';
+ *     protected $_has_one = array(
+ *		  'table2' => array(
+ *		    	'class'  => 'Table2',
+ *				'fk' 	 => 'table1_id')
+ *     );
+ * }
+ * class Table2 extends Mnix_ORM_Prototype {
+ *    protected $_table = 'table2';
+ * }
+ * //Тестируем
+ * $table1 = new Table1();
+ * $table2 = $table1->getTable2();
+ * </code>
+ * Table1 и Table4 в отношии N:N
+ * <code>
+ * class Table1 extends Mnix_ORM_Prototype
+ * {
+ *     protected $_table = 'table1';
+ *     protected $_has_many = array(
+ *		  'tables4' => array(
+ *              'class'  => 'Table2',
+ *              'jtable' => 'jtable',
+ *              'id'     => 'table4_id',
+ *              'fk' 	 => 'table1_id')
+ *     );
+ * }
+ * class Table4 extends Mnix_ORM_Prototype {
+ *    protected $_table = 'table4';
+ * }
+ * //Тестируем
+ * $table1 = new Table1();
+ * $tables4 = $table1->getTables4();
+ * </code>
+ *
+ * @category Mulanix
  * @package Mnix_ORM
  */
 abstract class Mnix_ORM_Prototype
 {
 	/**
      * Флаг загрузки
+     *
      * @var boolean
      */
-	protected $_isLoad   = false;
+	protected $_isLoad = false;
     /**
+     * Объект Mnix_Db_Select
+     *
      * @var object(Mnix_Db_Select)
      */
-	protected $_select   = null;
+	protected $_select = null;
     /**
      * Кортеж
+     *
      * @var array
      */
 	protected $_cortege;
-
+    /**
+     * Конструктор
+     *
+     * @param int $id
+     */
 	public function __construct($id = null)
     {
 		if (isset($id)) $this->_cortege['id'] = $id;
@@ -48,6 +93,8 @@ abstract class Mnix_ORM_Prototype
 	}
 	/**
      * Загрузка данных из бд
+     *
+     * return object($this)
      */
 	protected function _load()
     {
@@ -79,6 +126,7 @@ abstract class Mnix_ORM_Prototype
 	}
 	/**
      * Поиск, аналагочен методу where() из Mnix_Db_Select
+     *
      * @param string $condition
      * @param mixed $data
      * @return object(Mnix_ORM_Prototype)
@@ -92,6 +140,7 @@ abstract class Mnix_ORM_Prototype
     }
     /**
      * Параметры
+     *
      * @return array
      */
 	public function getParam()
@@ -124,6 +173,7 @@ abstract class Mnix_ORM_Prototype
     }
     /**
      * Get & Set методы
+     *
      * @param string $name
      * @param array $arg
      * @return mixed
@@ -156,6 +206,7 @@ abstract class Mnix_ORM_Prototype
 	}
 	/**
      * Запрос аттрибута
+     *
      * @param array $atts
      * @return mixed
      */
@@ -236,6 +287,7 @@ abstract class Mnix_ORM_Prototype
     }
     /**
      * Запрос парметров класса(таблица, поля и тп)
+     * 
      * @param string $name
      * @return array
      */
--- public/Mulanix/test/lib/ORM/CollectionTest.php
+++ public/Mulanix/test/lib/ORM/CollectionTest.php
@@ -1,15 +1,19 @@
 <?php
-class Test_Mnix_ORM_CollectionTest extends PHPUnit_Framework_TestCase {
-    public function __construct($name = NULL, array $data = array(), $dataName = '') {
+class Test_Mnix_ORM_CollectionTest extends PHPUnit_Framework_TestCase
+{
+    public function __construct($name = NULL, array $data = array(), $dataName = '')
+    {
         parent::__construct($name, $data, $dataName);
     }
-    public function testHasMany1() {
+    public function testHasMany1()
+    {
         $table1 = new Test_Mnix_ORM_Table1();
         $table1->find('id=?i', 1)->load();
         $tables3 = $table1->getTables3();
         $tables3->load();
         $i = 0;
-        foreach ($tables3 as $table3) {
+        foreach ($tables3 as $table3)
+        {
             $i++;
             //Защита при превышнии размера
             if ($i > 100) die();
@@ -21,7 +25,8 @@ class Test_Mnix_ORM_CollectionTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($val->getText(), 'text31');
         $this->assertEquals($key, 0);
     }
-    public function testHasMany2() {
+    public function testHasMany2()
+    {
         $table1 = new Test_Mnix_ORM_Table1();
         $tables3 = $table1->getTables3();
         $tables3->load();
@@ -40,12 +45,15 @@ class Test_Mnix_ORM_CollectionTest extends PHPUnit_Framework_TestCase {
         }
         $this->assertEquals($table2->getText(), 'text24');
     }
-    public function testHasMany2Many() {
+    public function testHasMany2Many()
+    {
         $table1 = new Test_Mnix_ORM_Table1(2);
         $tables4 = $table1->getTables4();
         $tables4->load();
         foreach ($tables4 as $table4) {
             $this->assertEquals($table4->getText(), 'text42');
         }
+        //JOIN
+
     }
 }
