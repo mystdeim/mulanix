ad1970776c80d09757671ece424f3839acc69928
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-04 17:20:20 +0000

    * Создана новая тестовая таблица mnix_test_uri2 * Упрощен конечный автомат для Mnix_Uri, теперь в нём 7 состояний * Конечный автомат работает теперь по таблице mnix_test_uri2 *! Существует потенциальная проблема, если в таблице будут записи имеющие общих предков, в которых поле obligate равно нулю

--- public/Mulanix/lib/Uri.php
+++ public/Mulanix/lib/Uri.php
@@ -27,7 +27,7 @@ class Mnix_Uri extends Mnix_ORM_Prototype
      * @param string $data
      * return int
      */
-    protected static function _parse($data = null)
+    /*protected static function _parse($data = null)
     {
         if (isset($data)) $requests = self::_parts($data);
         else $requests = self::_parts($_SERVER['REQUEST_URI']);
@@ -143,6 +143,96 @@ class Mnix_Uri extends Mnix_ORM_Prototype
             if ($iii > 100) {
                 //TODO
                 //Тут кидать исключение
+                break;
+            }
+        } while ($state);
+        return $id;
+    }*/
+    protected static function _parse($data = null)
+    {
+        if (isset($data)) $requests = self::_parts($data);
+        else $requests = self::_parts($_SERVER['REQUEST_URI']);
+        $db = Mnix_Db::connect();
+        $state = 1;
+        $data = array();
+        $parent = 0;
+        $iii = 0;
+        do {
+            //var_dump('State:'.$state);
+            switch ($state) {
+                //Берём слово из урл
+                case 1:
+                    $request = array_shift($requests);
+                    if ($request) {
+                        $state = 2;
+                    }
+                    else $state = 0;
+                    break;
+                //Берём урл
+                case 2:
+                    $res_t = $db->select()->from('mnix_test_uri2', '*')
+                                          ->where('?t = ?i', array('parent', $parent))
+                                          ->query();
+                    if (isset($res) && count($res)) $res = array_merge($res_t, $res);
+                    else $res = $res_t;
+                    if ($res) {
+                        //var_dump($res);
+                        $uri = array_shift($res);
+                        $state = 3;
+                    } else $state = 5;
+                    break;
+                //Проверяем обязательность
+                case 3:
+                    if ($uri['obligate']) {
+                        $parent = $uri['id'];
+                        $id = $uri['page_id'];
+                        $state = 4;
+                    } else $state = 6;
+                    break;
+                //Проверяем регулярку
+                case 4:
+                    $pattern = '/^' . $uri['regular'] . '$/';
+                    if (preg_match($pattern, $request)) {
+                        $parent = $uri['id'];
+                        $id = $uri['page_id'];
+                        $state = 1;
+                    } else $state = 7;
+                    break;
+                //Тут выдаём ошибочную страницу
+                case 5:
+                    $res = $db->select()->from('mnix_test_uri2', '*')
+                                        ->where('?t = ?i', array('parent', -1))
+                                        ->query();
+                    $id = $res[0]['page_id'];
+                    $state = 0;
+                    break;
+                //Проверяем регулярку
+                case 6:
+                    $pattern = '/^' . $uri['regular'] . '$/';
+                    $parent = $uri['id'];
+                    if (preg_match($pattern, $request)) {
+                        $id = $uri['page_id'];
+                        $state = 1;
+                    } else $state = 2;
+                    break;
+                //Смотрим остались ли урлы
+                case 7:
+                    $uri = array_shift($res);
+                    if ($uri) $state = 3;
+                    else $state = 5;
+                    break;
+                //TODO
+                //Тут кидать исключение
+                default:
+                    var_dump('NO state!'.$state);
+                    break;
+            }
+            //Типa защита
+            $iii++;
+            if ($iii > 100) {
+                //TODO
+                //Тут кидать исключение
+                var_dump('TO MANY!');
                 break;
             }
         } while ($state);
