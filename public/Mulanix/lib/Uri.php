a2edcafccd074d4fc458843926f17546b1270545
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-04 17:36:50 +0000

    * Исправлена ошибка в  конечном автомате для Mnix_Uri * Количество сравнений в тестах превысило 100, на данный момент: систему проверяет 78 тестов в 105 сравнениях

--- public/Mulanix/lib/Uri.php
+++ public/Mulanix/lib/Uri.php
@@ -27,127 +27,6 @@ class Mnix_Uri extends Mnix_ORM_Prototype
      * @param string $data
      * return int
      */
-    /*protected static function _parse($data = null)
-    {
-        if (isset($data)) $requests = self::_parts($data);
-        else $requests = self::_parts($_SERVER['REQUEST_URI']);
-        $db = Mnix_Db::connect();
-        $state = 1;
-        $data = array();
-        $parent = 0;
-        $iii = 0;
-        do {
-            //var_dump('State:'.$state);
-            switch ($state) {
-                //Берём слово из урл
-                case 1:
-                    $request = array_shift($requests);
-                    if ($request) $state = 2;
-                    else $state = 0;
-                    break;
-                //Получаем группы
-                case 2:
-                    $res = $db->select()->from('mnix_test_uri', '*')
-                                        ->where('?t = ?i', array('parent', $parent))
-                                        ->query();
-                    if ($res) {
-                        foreach ($res as $key => $val) $data[$val['group']][$val['order']] = $val;
-                        unset($res);
-                        //var_dump($data);
-                        $group = array_shift($data);
-                        $state = 3;
-                    } else $state = 8;
-                    break;
-                //Берём урл из группы
-                case 3:
-                    $uri = array_shift($group);
-                    if ($uri) $state = 4;
-                    else $state = 8;
-                    break;
-                //Проверяем обязательность
-                case 4:
-                    if ($uri['obligate']) {
-
-                        $parent = $uri['group'];
-                        $id = $uri['page_id'];
-
-                        $state = 5;
-                    } else $state = 9;
-                    break;
-                //Проверяем регулярку
-                case 5:
-                    $pattern = '/^' . $uri['regular'] . '$/';
-                    if (preg_match($pattern, $request)) {
-                        $parent = $uri['group'];
-                        $id = $uri['page_id'];
-                        $state = 6;
-                    } else $state = 12;
-                    break;
-                //Если урлов нет, то в 1
-                case 6:
-                    $uri = array_shift($group);
-                    if ($uri) $state = 7;
-                    else $state = 1;
-                    break;
-                //Смотрим остались ли запросы
-                case 7:
-                    $request = array_shift($requests);
-                    if ($request) $state = 4;
-                    else $state = 0;
-                    break;
-                //Тут выдаём ошибочную страницу
-                case 8:
-                    $res = $db->select()->from('mnix_test_uri', '*')
-                                        ->where('?t = ?i', array('group', -1))
-                                        ->query();
-                    $id = $res[0]['page_id'];
-                    $state = 0;
-                    break;
-                //Проверяем регулярку
-                case 9:
-                    //var_dump($uri);
-                    $pattern = '/^' . $uri['regular'] . '$/';
-                    $parent = $uri['group'];
-                    $id = $uri['page_id'];
-                    if (preg_match($pattern, $request)) {
-                        $state = 6;
-                    } else $state = 10;
-                    break;
-                //Если урлов нет, то в 11
-                case 10:
-                    $uri = array_shift($group);
-                    if ($uri) $state = 4;
-                    else $state = 11;
-                    break;
-                //Проверяем остались ли группы
-                case 11:
-                    $group = array_shift($data);
-                    if ($group) $state = 3;
-                    else $state = 2;
-                    //exit();
-                    break;
-                //Проверяем остались ли группы
-                case 12:
-                    $group = array_shift($data);
-                    if ($group) $state = 3;
-                    else $state = 8;
-                    break;
-                //TODO
-                //Тут кидать исключение
-                default:
-                    var_dump('NO state!'.$state);
-                    break;
-            }
-            //Типa защита
-            $iii++;
-            if ($iii > 100) {
-                //TODO
-                //Тут кидать исключение
-                break;
-            }
-        } while ($state);
-        return $id;
-    }*/
     protected static function _parse($data = null)
     {
         if (isset($data)) $requests = self::_parts($data);
@@ -156,12 +35,13 @@ class Mnix_Uri extends Mnix_ORM_Prototype
         $state = 1;
         $data = array();
         $parent = 0;
-        $iii = 0;
+        $i = 0;
         do {
-            //var_dump('State:'.$state);
+            //var_dump('State: ' . $state);
             switch ($state) {
                 //Берём слово из урл
                 case 1:
+                    $res = array();
                     $request = array_shift($requests);
                     if ($request) {
                         $state = 2;
@@ -175,8 +55,8 @@ class Mnix_Uri extends Mnix_ORM_Prototype
                                           ->query();
                     if (isset($res) && count($res)) $res = array_merge($res_t, $res);
                     else $res = $res_t;
+                    //var_dump($res);
                     if ($res) {
-                        //var_dump($res);
                         $uri = array_shift($res);
                         $state = 3;
                     } else $state = 5;
@@ -228,8 +108,8 @@ class Mnix_Uri extends Mnix_ORM_Prototype
                     break;
             }
             //Типa защита
-            $iii++;
-            if ($iii > 100) {
+            $i++;
+            if ($i > 100) {
                 //TODO
                 //Тут кидать исключение
                 var_dump('TO MANY!');
