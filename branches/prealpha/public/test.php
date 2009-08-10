890029fd12e03bd33a96932af343ea7621594d6f
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-10 17:52:02 +0000

    * Сохраниние перед изменением структуры проекта

--- branches/prealpha/public/test.php
+++ branches/prealpha/public/test.php
@@ -9,7 +9,19 @@
  * @author deim
  * Created on 24.04.2009, 11:01:50
  */
-require_once 'Mulanix/boot/firstconfig.php';
+define('MNIX_STARTTIME', microtime(true));
+define('MNIX_PROJECT', realpath('./'));
+define('MNIX_DIR', MNIX_PROJECT . '/Mulanix/');
+define('MNIX_LIB', MNIX_DIR . 'lib/');
+define('MNIX_TEST', MNIX_DIR . 'test/');
+define('MNIX_BOOT', MNIX_DIR . 'boot/');
+define('MNIX_TMP', MNIX_DIR . 'tmp/');
+define('MNIX_CACHE', MNIX_DIR . 'tmp/cache/');
+define('MNIX_THEME', MNIX_DIR . 'theme/');
+define('MNIX_CONFIG', MNIX_BOOT . 'config.xml');
+if (!defined('PATH_SEPARATOR')) define('PATH_SEPARATOR', getenv('COMSPEC')? ';' : ':');
+ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.MNIX_DIR);
+require_once MNIX_LIB . 'Core.php';
 require_once 'Mulanix/test/lib/Core.php';
 $app = new Test_Mnix_Core();
 $app->run();
