91a6a5e85392565ca0452e6ca1f8d11e842a6dc7
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-10 19:07:39 +0000

    * Изменением структуры проекта

--- public/Mulanix/component/Template/model/Component.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Template_model_Component extends lib_ORM_Prototype {
-	protected $_table = 'component';
-	protected $_has_many = array(
-		'templates' => array(
-				'class' => 'Template_model_Template',
-				'key' 	=> 'component_id'));
-}
--- public/Mulanix/lib/Static/template/admin.xsl
+++
@@ -1,5 +0,0 @@
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:template match="admin">
-        <p>It`s Admin!</p>
-    </xsl:template>
-</xsl:stylesheet>
