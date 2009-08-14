0e47483234f89fa768a7a9d4de373cddd692fc70
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-14 09:43:59 +0000

    * Тестирую автобавление версии

---
+++ branches/prealpha/test/lib/Mnix/PHPClass.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Mulanix Framework
+ *
+ * @category Mulanix
+ * @package Mnix_
+ * @version SVN: $Id$
+ * @since 2009-
+ */
+/**
+ * @category Mulanix
+ * @package Mnix_
+ */
+class PHPClass
+{
+
+}
