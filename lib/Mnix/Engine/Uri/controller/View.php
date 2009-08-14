3725af28935a519c343c23606a1e303b93ecbe77
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-14 14:40:19 +0000

    * Бекап БД * Добавил страницу URI * Изменил title на name в БД * Поправил Mnix_Uri вследствии пред пункта

---
+++ lib/Mnix/Engine/Uri/controller/View.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Mulanix Framework
+ *
+ * @category Mulanix
+ * @package Mnix_Engine
+ * @version $Id$
+ */
+/**
+ * @category Mulanix
+ * @package Mnix_Engine
+ * @subpackage Mnix_Engine_Uri
+ */
+class Mnix_Engine_Uri_controller_View extends Mnix_Core_Controller
+{
+    public function run()
+    {
+        
+    }
+}
