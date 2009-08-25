bc6f8184a3a75637e13e46ab32d804217d9d1fa5
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2009-08-25 16:31:20 +0000

    * Изменение структуры тестирования проекта

--- public/test.php
+++ public/test.php
@@ -1,27 +1,20 @@
 <?php
 /**
- * Тестирование
+ * Mulanix Framework
  *
- * Для проведения всех тестов: запустить http://localhost/test
- * Для конкретного теста: http://localhost/test/__CLASS__
- * где __CLASS__ названия определённого классна, например: Test_Mnix_DbTest
- * 
- * @author deim
- * Created on 24.04.2009, 11:01:50
+ * @category Mulanix
+ * @package Mnix
+ * @subpackage Test
+ * @version $Id$
+ * @author mystdeim <mysteim@gmail.com>
  */
-define('MNIX_CORE_STARTTIME', microtime(true));
-define('MNIX_PATH_DIR', realpath('../').'/');
-define('MNIX_PATH_LIB', MNIX_PATH_DIR . 'lib/');
-define('MNIX_PATH_TEST', MNIX_PATH_DIR . 'test/');
-define('MNIX_PATH_BOOT', MNIX_PATH_DIR . 'boot/');
-define('MNIX_PATH_TMP', MNIX_PATH_DIR . 'tmp/');
-define('MNIX_PATH_VAR', MNIX_PATH_DIR . 'var/');
-define('MNIX_PATH_CACHE', MNIX_PATH_DIR . 'tmp/cache/');
-define('MNIX_PATH_PUBLIC', MNIX_PATH_DIR . 'htdocs/');
-define('MNIX_PATH_THEME', MNIX_PATH_PUBLIC . 'theme/');
-define('MNIX_PATH_CONFIG', MNIX_PATH_BOOT . 'config.xml');
-if (!defined('PATH_SEPARATOR')) define('PATH_SEPARATOR', getenv('COMSPEC')? ';' : ':');
-ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.MNIX_PATH_DIR);
+ /**
+  * Подключаем файл начальной загрузки
+  */
+require_once '../boot/bootstrap.php';
+/**
+ * Подключаем ядро
+ */
 require_once MNIX_PATH_LIB . 'Mnix/Core.php';
 require_once MNIX_PATH_TEST . 'lib/Mnix/Core.php';
 $app = new Test_Mnix_Core();
