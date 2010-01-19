70efc7c48b137d8165290baa294a5497b0e64706
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-01-19 19:24:03 +0000

    * xml

--- public/t1.php
+++ public/t1.php
@@ -1,32 +1,5 @@
 <?php
 
-
-require_once '../test/Mnix/CoreSub.php';
-//require_once '../test/Mnix/CacheSub.php';
-
-$core = \Mnix\Core::instance();
-
-class B
-{
-    public function b1()
-    {
-        //\Mnix\Core::instance()->log('s', 'AAAAA');
-        $b = new \Mnix\Cache();
-        var_dump($b->dir());
-
-    }
-}
-
-class A
-{
-    public function __construct()
-    {
-        $b = new B();
-        $b->b1();
-    }
-}
-
-
-$a = new A();
-var_dump(Mnix\Core::instance());
-//echo '<pre>'.$core->_log.'</pre>';
+$a = 50;
+var_dump(eval ('if ($a>5 AND $a < 100) return true; else return false;'));
+throw new Exception();
