1482b9a99307bcfdf863af6bf6fdc29d48feee64
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-01-23 12:04:34 +0000

    * Написан метод table() к Mnix\Db\Xml\Select и протестирован

--- branches/alpha/lib/Mnix/Db/Xml/Select.php
+++ branches/alpha/lib/Mnix/Db/Xml/Select.php
@@ -12,19 +12,65 @@ namespace Mnix\Db\Xml;
  */
 class Select extends \Mnix\Db\Base implements \Mnix\Db\iSelect
 {
-    protected $_query;
-
+    /**
+     *
+     * @var array
+     */
+    protected $_tables = array();
+    /**
+     *
+     * @param mixed $table
+     * @param mixed $column
+     * @return object(\Mnix\Db\Xml\Select)
+     */
     public function table($table, $column = null)
     {
-        $this->_query = '/root/' . $table . '/item';
-        /*if (isset($column) && $column !== '*') {
-            $this->_query .= '[@'.$column.']';
-        }*/
+        if (!is_array($table)) $this->_tables[$table] = null;
+        else {
+            $this->_tables[key($table)]['alias'] = current($table);
+            $table = key($table);
+        }
+        if (isset($column)) {
+            if (!is_array($column)) $columns = array($column => null);
+            else {
+                if (is_int(key($column))) {
+                    foreach ($column as $key => $val) $columns[$val] = null;
+                } else $columns = $column;
+            }
+        }
+        $this->_tables[$table]['columns'] = $columns;
         return $this;
     }
+    /**
+     *
+     * @return array
+     */
     public function execute()
     {
-        $result = $this->_driver->query($this->_query);
+        //Обходим таблицы из которых бцдет выборка
+        foreach ($this->_tables as $tableName => $tableValue) {
+            $tableResult = $this->_driver->query('/root/' . $tableName . '/item');
+            //Если существуют столбцы, то смотрим их
+            if (isset($tableValue['columns'])) {
+                //Если * то пишем всё
+                if (array_key_exists('*', $tableValue['columns'])) $result = array_merge($tableResult);
+                else {
+                    //Обходим каждую строчку результата
+                    foreach($tableResult as $itemName => $itemValue) {
+                        //Обходим каждый столбец
+                        foreach ($itemValue as $attrName => $attrValue) {
+                            // и выбираем нужные столбцы
+                            if (array_key_exists($attrName, $tableValue['columns'])) {
+                                //Учитываем алиасы столбцов
+                                if (!isset($tableValue['columns'][$attrName])) $item[$attrName] = $attrValue;
+                                else $item[$tableValue['columns'][$attrName]] = $attrValue;
+                            }
+                        }
+                        $result[] = $item;
+                    }
+                }
+            }
+        }
         return $result;
     }
 }
