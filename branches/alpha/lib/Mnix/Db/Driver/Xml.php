6484517f03194639793e72d4956b5613b3ffe0fa
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-01-28 21:40:03 +0000

    * Написан и протестирован Mnix\Db\Xml\Delete

--- branches/alpha/lib/Mnix/Db/Driver/Xml.php
+++ branches/alpha/lib/Mnix/Db/Driver/Xml.php
@@ -41,14 +41,12 @@ class Xml
      * @param string $query
      * @return array
      */
-    public function query($query)
+    /*public function query($query)
     {
         if (!isset($this->_dom)) $this->_load();
         $xpath = new \DOMXPath($this->_dom);
         $nodeList = $xpath->query($query);
-        //var_dump($nodeList);
         $result = array();
-        //var_dump($nodeList->ownerDocument->saveXML($nodeList));
         foreach ($nodeList as $domElement){
             $attributes = $domElement->attributes;
             //var_dump($domElement->ownerDocument->saveXML($domElement));
@@ -58,6 +56,22 @@ class Xml
             $result[] = $node;
         }
         return $result;
+    }*/
+    public function query($query)
+    {
+        //var_dump($query);
+        if (!isset($this->_dom)) $this->_load();
+        $xpath = new \DOMXPath($this->_dom);
+        return $xpath->query($query);
+    }
+    /**
+     * Сохраняет изменения дом-структуры
+     */
+    public function save()
+    {
+        $this->_dom->save($this->_file);
+        //Сбрасываем дом, так как файл уже изменён
+        $this->_dom = null;
     }
     /**
      * Загрузка xml-файла
@@ -65,6 +79,10 @@ class Xml
     protected function _load()
     {
         $this->_dom = new \DOMDocument();
+        //Игнорируем всякие пробелы, переводы строк
+        $this->_dom->preserveWhiteSpace = false;
+        //Форматируем с отступами
+        $this->_dom->formatOutput = true;
         $this->_dom->load($this->_file);
     }
 }
---
+++ branches/alpha/test/Mnix/Db/Xml/_files/DeleteSub.php
@@ -0,0 +1,25 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Mnix\Db\Xml;
+
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/boot/bootstrap.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Exception.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Core.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Db.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Db/Base.php';
+//require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Db/iSelect.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Db/Xml/Base.php';
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/lib/Mnix/Db/Xml/Delete.php';
+require_once dirname(dirname(__DIR__)) . '/Driver/_files/XmlSub.php';
+/**
+ * Description of DeleteSub
+ *
+ * @author deim
+ */
+class DeleteSub extends \Mnix\Db\Xml\Delete
+{
+    //put your code here
+}
