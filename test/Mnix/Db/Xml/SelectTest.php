40a3fa4e59e3f38f773d1af9ed35a265bc5af053
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-02-01 11:31:14 +0000

    * Реализовал LEFT JOIN для XML

--- test/Mnix/Db/Xml/SelectTest.php
+++ test/Mnix/Db/Xml/SelectTest.php
@@ -61,17 +61,20 @@ class Mnix_Db_Xml_SelectTest extends PHPUnit_Framework_TestCase
         return array(
             array('table1', '*', array(
                                     array('id'=>'1', 'attr1'=>'a1', 'attr2'=>'b1'),
-                                    array('id'=>'2', 'attr1'=>'a2', 'attr2'=>'b2')
+                                    array('id'=>'2', 'attr1'=>'a2', 'attr2'=>'b2'),
+                                    array('id'=>'3', 'attr1'=>'a3', 'attr2'=>'b3')
                                 )
             ),
             array('table1', 'id', array(
                                     array('id'=>'1'),
-                                    array('id'=>'2')
+                                    array('id'=>'2'),
+                                    array('id'=>'3')
                                 )
             ),
             array('table1', array('id'=>'i', 'attr1'=>'a'), array(
                                     array('i'=>'1', 'a'=>'a1'),
-                                    array('i'=>'2', 'a'=>'a2')
+                                    array('i'=>'2', 'a'=>'a2'),
+                                    array('i'=>'3', 'a'=>'a3')
                                 )
             )
         );
@@ -80,7 +83,7 @@ class Mnix_Db_Xml_SelectTest extends PHPUnit_Framework_TestCase
      *
      * @dataProvider providerWhere
      */
-    /*public function testWhere($condition, $data, $result)
+    public function testWhere($condition, $data, $result)
     {
         $select = new Mnix\Db\Xml\SelectSub($this->_driver);
         $data = $select->table('table1', '*')
@@ -96,27 +99,72 @@ class Mnix_Db_Xml_SelectTest extends PHPUnit_Framework_TestCase
                                 )
             )
         );
-    }*/
-    /**
-     * 
-     */
-    /*public function testJoin()
+    }
+    public function testJoin()
     {
         $select = new Mnix\Db\Xml\SelectSub($this->_driver);
-        $select->table('table1', 'id')
+        $result = $select->table('table1', 'id')
                        ->join(array('table2'    => 'table1'),
                               array('table1_id' => 'id'),
-                              array('id' => 'a.id', 'attr22' => 'a.attr22'));
-
-        $result = $select->execute();
+                              array('id' => 'a.id', 'attr22' => 'a.attr22'))
+                       ->execute();
         $expectedResult = array(
-            0 => array(
+            array(
                 'id'       => 1,
                 'a.id'     => 1,
                 'a.attr22' => 'c1'
+            ),
+            array(
+                'id'       => 1,
+                'a.id'     => 2,
+                'a.attr22' => 'c2'
+            ),
+            array(
+                'id'       => 2,
+                'a.id'     => 3,
+                'a.attr22' => 'c3'
+            ),
+            array(
+                'id'       => 2,
+                'a.id'     => 4,
+                'a.attr22' => 'c4'
+            ),
+            array(
+                'id'       => 3
             )
         );
-
-
-    }*/
+        $this->assertEquals($result, $expectedResult);
+    }
+    public function testJoin2()
+    {
+        $select = new Mnix\Db\Xml\SelectSub($this->_driver);
+        $result = $select->table('table2', array('id', 'attr22'))
+                       ->join(array('table1'    => 'table2'),
+                              array('id' => 'table1_id'),
+                              array('attr2' => 'a.attr2'))
+                       ->execute();
+        $expectedResult = array(
+            array(
+                'id'      => 1,
+                'attr22'  => 'c1',
+                'a.attr2' => 'b1'
+            ),
+            array(
+                'id'       => 2,
+                'attr22'  => 'c2',
+                'a.attr2' => 'b1'
+            ),
+            array(
+                'id'       => 3,
+                'attr22'  => 'c3',
+                'a.attr2' => 'b2'
+            ),
+            array(
+                'id'       => 4,
+                'attr22'  => 'c4',
+                'a.attr2' => 'b2'
+            )
+        );
+        $this->assertEquals($result, $expectedResult);
+    }
 }
