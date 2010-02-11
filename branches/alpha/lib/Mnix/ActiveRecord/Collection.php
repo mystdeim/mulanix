cad313d8b0142a933b2dfb149be15253b2d3a304
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-02-11 18:29:55 +0000

    * ActiveRecord

---
+++ branches/alpha/lib/Mnix/ActiveRecord/Collection.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Mulanix Framework
+ */
+namespace Mnix\ActiveRecord;
+/**
+ * Mulanix Framework
+ *
+ * @author deim
+ */
+class Collection extends \ArrayObject
+{
+    //put your code here
+}
