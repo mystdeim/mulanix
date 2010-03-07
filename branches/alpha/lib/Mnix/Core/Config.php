a8f9d43544cce184adcf669ae05df66eda75ab4c
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-03-07 20:19:56 +0000

    * Update index * Create new config

---
+++ branches/alpha/lib/Mnix/Core/Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Mulanix Framework
+ */
+namespace Mnix;
+/**
+ * Mulanix Framework
+ *
+ * @author deim
+ */
+class Config
+{
+    //put your code here
+}
---
+++ branches/alpha/test/Mnix/ConfigTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Mulanix Framework
+ */
+namespace Mnix;
+/**
+ * Mulanix Framework
+ *
+ * @author deim
+ */
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    //put your code here
+}
