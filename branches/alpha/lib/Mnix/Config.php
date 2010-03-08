4b74fb2cd8e345d10324e1d5a8302d7c2fa1864d
mystdeim <mystdeim@fd3a51fa-76e3-11de-abb5-1d1887d8974b>
2010-03-08 08:52:02 +0000

    * Update Mnix\Config

--- branches/alpha/lib/Mnix/Config.php
+++ branches/alpha/lib/Mnix/Config.php
@@ -10,7 +10,9 @@ namespace Mnix;
  */
 class Config
 {
-    public static function load()
+    //const FILE = Path\CONFIG;
+    protected $_path;
+    /*public static function load()
     {
         $cache = new Cache();
         $cache->file(Path\CONFIG)
@@ -20,5 +22,24 @@ class Config
             $writer = new Mnix_Config_Writer(&$cache);
         }
         require_once $cache->path();
+    }*/
+    public function __construct($path)
+    {
+        $this->_path = $path;
+    }
+    public function load()
+    {
+        $cache = new Cache();
+        $cache->name(md5_file($this->_path))
+              ->hash(false);
+        if (!$cache->load()) {
+            var_dump($cache);
+            $this->_write($cache);
+        }
+    }
+    protected function _write($cache)
+    {
+        $cache->data('test')
+              ->save();
     }
 }
---
+++ branches/alpha/test/Mnix/_files/ConfigSub.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Mulanix Framework
+ */
+namespace Mnix;
+
+require_once dirname(dirname(__DIR__)) . '/Helper.php';
+require_once \Mnix\Path\LIB . '/Mnix/Core.php';
+require_once \Mnix\Path\LIB . '/Mnix/Exception.php';
+require_once \Mnix\Path\LIB . '/Mnix/Cache.php';
+require_once \Mnix\Path\LIB . '/Mnix/Config.php';
+/**
+ * Mulanix Framework
+ *
+ * @author deim
+ */
+class ConfigSub extends Config
+{
+    const FILE = '';
+}
